acfef31df9669f3d73d008e33d35d2b2ba93d214
raemerrr <foals18@gmail.com>
2021-11-12 13:10:16 +0900

    test: UTF-8 필터 추가

--- demo/src/test/java/com/api/demo/PublicApiRestControllerTest.java
+++ demo/src/test/java/com/api/demo/PublicApiRestControllerTest.java
@@ -2,11 +2,15 @@ package com.api.demo;
 
 import com.api.demo.apis.PublicApiRestController;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -17,6 +21,17 @@ public class PublicApiRestControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @BeforeEach
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(wac)
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))  // 필터 추가
+                .alwaysDo(print())
+                .build();
+    }
 
     @Test
     public void callApi() throws Exception {
